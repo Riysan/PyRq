4eb91922c7aa848403eb5264b373bcae50457154
Riysan <67790884+Riysan@users.noreply.github.com>
2020-11-02 00:28:25 +0900

    v1.0.0

--- pyrq/__init__.py
+++ pyrq/__init__.py
@@ -1,1 +1,31 @@
-h
+import requests
+LIMELE_API_URL = "https://riyblog.shop/limele/api/v2/"
+
+def text(url):
+    if url == "":
+        return "null"
+    else:
+        res = requests.get(url)
+        return str(res.text)
+
+def json(url):
+    if url == "":
+        return "null"
+    else:
+        res = requests.get(url)
+        return res.json()
+
+def limele(url):
+    if url == "":
+        return "null"
+    else:
+        res = requests.get(LIMELE_API_URL + "?url=" + url)
+        r = res.json()
+        if r["msg"] == "404":
+            print("サーバー又は通信エラーです")
+            return "err"
+        elif r["msg"] == "200":
+            return r["url"]
+        else:
+            print("例外が発生しました")
+            return "err"

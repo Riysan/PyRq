0cbdc0d63696e43d0bc4112965e5b3d8d3a04709
Riysan <67790884+Riysan@users.noreply.github.com>
2020-11-02 00:29:17 +0900

    setting

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setuptools.setup(
     name="pyrq",
     version="1.0.0",
     author="riysan",
+    install_requires = ["requests"],
     url="https://github.com/Riysan/PyRq/",
     packages=setuptools.find_packages(),
     license = "MIT"

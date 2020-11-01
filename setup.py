afa3bddb3a6856ec740bca78e0a3d6cbfb31c941
Riysan <67790884+Riysan@users.noreply.github.com>
2020-11-02 00:50:16 +0900

    パッケージ

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import setuptools
  
 setuptools.setup(
-    name="pyrq",
-    version="1.0.0",
-    author="riysan",
+    name = "pyrq",
+    version = "1.0.0",
+    author = "riysan",
     install_requires = ["requests"],
-    url="https://github.com/Riysan/PyRq/",
-    packages=setuptools.find_packages(),
+    url = "https://github.com/Riysan/PyRq/",
+    packages = ["pyrq"],
+    download_url = 'https://riysan.github.io/PyRq/pkg/pyrq-1.0.0.tar.gz',
     license = "MIT"
 )

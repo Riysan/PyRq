47f07b9fb7ce420a9e1c66663e791a9db2906d0e
Riysan <67790884+Riysan@users.noreply.github.com>
2020-11-01 23:12:37 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+ 
+setuptools.setup(
+    name="pyrq",
+    version="1.0.0",
+    author="riysan",
+    url="https://github.com/Riysan/PyRq/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7.14",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)

706b5019c2fe8ac53138d589552e411fba9ba039
Riysan <67790884+Riysan@users.noreply.github.com>
2020-11-01 23:38:16 +0900

    setting

--- setup.py
+++ setup.py
@@ -6,9 +6,5 @@ setuptools.setup(
     author="riysan",
     url="https://github.com/Riysan/PyRq/",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 2.7.14",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ]
+    license = "MIT"
 )
